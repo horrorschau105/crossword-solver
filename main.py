6a7d8b5680bd0a0b3494cad6f352f6f8bfa3434d
horrorschau105 <piotrekgdowski@wp.pl>
2019-04-24 11:36:49 +0200

    refactor, cleanings

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
 import fastText
-from fastText import util
 import numpy as np
+from fastText import util
+from collections import namedtuple
 
-def get_holes(grid, h, w):
+HolePosition = namedtuple("HolePosition", "x y direction length")
+Hole = namedtuple("Hole", "position clue")
 
-    holes = {}
+def compare_plain_holes(hole_position1, hole_position2):
+    if hole_position1.x < hole_position2.x:
+        return -1
+    if hole_position1.x == hole_position2.x and hole_position1.y < hole_position2.y:
+        return -1
+    return 1
+
+def get_cells(hp):
+    cells = []
+    for i in range(hp.length):
+        if hp.direction == 'vertical':
+            cells.append((hp.x + i, hp.y))
+        else:
+            cells.append((hp.x, hp.y + i))
+
+    return cells
+
+def get_connected_holes(fixed, holes):
+
+    count = 0
+    fixed_cells = get_cells(fixed.position)
+
+    for hole in holes:
+        if hole.position.direction == fixed.position.direction:
+            continue
+        
+        for cell in get_cells(hole.position):
+            if cell in fixed_cells:
+                count += 1
+                break
+ 
+    return (count, -fixed.position.length)
+
+def get_positions(grid, h, w):
+    holes = []
     for i in range(h):
         for j in range(w):
             if (i == 0 or grid[i-1][j] == '#') and grid[i][j] == '.' :
@@ -14,7 +50,7 @@ def get_holes(grid, h, w):
                     length += 1
                     index += 1
                 if length > 1:
-                    holes[(i,j, 'vertical')] = length
+                    holes.append(HolePosition(i, j, 'vertical', length))
             if (j == 0 or grid[i][j-1] == '#') and grid[i][j] == '.' :
                 length = 0
                 index = j
@@ -22,7 +58,7 @@ def get_holes(grid, h, w):
                     length += 1
                     index += 1
                 if length > 1:
-                    holes[(i,j, 'horizontal')] = length
+                    holes.append(HolePosition(i, j, 'horizontal', length))
     return holes
 
 def find_nearest_words_ids_for_hint(model, text):
@@ -52,43 +88,62 @@ def get_word_list(wordmap, ids, word_length):
     """
 
     result_list = []
-    for id in ids:
+
+    for (id, _) in ids:
         if len(wordmap[id][0]) == word_length:
             result_list.append(wordmap[id][0])
 
     return result_list
 
 if __name__ == "__main__":
-    lines = ""
-    with open("input.txt") as file_input:
-        lines = [line[:-1] for line in file_input.readlines()]
-    test_cases, input = int(lines[0]), lines[1:]
-    for i in range(test_cases):
-        h, w = [int(n) for n in input[0].split(' ')]
-        input = input[1:]
-        grid, input = input[:h], input[h:]
-        hhc, input = int(input[0]), input[1:] # horizontal hints and count
-        hh, input = input[:hhc], input[hhc:]
-        vhc, vh = int(input[0]), input[1:] # vertical hints and count
-
-        holes = get_holes(grid, h, w)
-        if len([key for key in holes if key[2] == 'vertical']) != vhc or len([key for key in holes if key[2] == 'horizontal']) != hhc:
-            print "Cos jest nie tak z krzyzowka"
-
-    print "Loading model..."
+    print "Loading model..."    
     
-    f = fastText.load_model("../result/fil1.bin")
+    f = fastText.load_model("../result/fil5.bin")
 
     wordmap = {}
     index = 0
     for w in f.get_words():
         wordmap[index] = (w, f.get_word_vector(w))
         index += 1
-    result_vector = find_nearest_words_ids_for_hint(f, "Sneezy, Grumpy or Happy")
 
-    word_list = get_word_list(wordmap, [i[0] for i in result_vector], 5)
+    print "Parsing crossword..."
+    lines = ""
 
-# okej, no to trzeba wypalic siec w takim razie
+    with open("input.txt") as file_input:
+        lines = [line[:-1] for line in file_input.readlines()]
+    
+    h, w = [int(n) for n in lines[0].split(' ')]
+    lines = lines[1:]
+    grid, lines = lines[:h], lines[h:]
+    hhc, lines = int(lines[0]), lines[1:] # horizontal hints and count
+    hh, lines = lines[:hhc], lines[hhc:]
+    vhc, vh = int(lines[0]), lines[1:] # vertical hints and count
 
-# solver ---> TBD
+    positions = get_positions(grid, h, w)
+    if len([key for key in positions if key.direction == 'vertical']) != vhc or len([key for key in positions if key.direction == 'horizontal']) != hhc:
+        print "Cos jest nie tak z krzyzowka"
 
+    sorted_positions = sorted(positions, cmp=compare_plain_holes)
+
+    holes = []
+    v_idx, h_idx = 0, 0
+    for position in sorted_positions:
+        if position.direction == 'vertical':
+            holes.append(Hole(position, vh[v_idx]))
+            v_idx += 1
+        else:
+            holes.append(Hole(position, hh[h_idx]))
+            h_idx += 1
+
+    holes = sorted(holes, key=lambda hole: get_connected_holes(hole, holes))
+    for hole in holes:
+        word_list = get_word_list(wordmap, find_nearest_words_ids_for_hint(f, hole.clue), hole.position.length)
+        print word_list[:10]
+
+# pomysl na solver:
+# * przesortuj hasla po stopniu ich uwiklania malejaco
+# * pilotazowo: wpisz jedno slowo w pierwsza dziure i pusc A*
+# * jak nie pyknie, to nastepne
+# * ryzyko jest takie, ze pojedyncza iteracja A* zajmie duzo czasu
+
+# siec wypalona, trzeba nieco posprzatac i solver napisac
